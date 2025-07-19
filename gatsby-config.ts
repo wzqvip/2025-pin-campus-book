45a732b08aad8c8a0abc92e1115674c52510e313
Taco <wangziqi2002vip@163.com>
2025-07-19 15:47:02 +0800

    Update gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,7 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  pathPrefix: `/`,
+  pathPrefix: `/2025-pin-campus-book`,
   siteMetadata: {
     title: `Pin Campus Book`,
     description: `上科大2025届新生纪念可交互礼物`,
